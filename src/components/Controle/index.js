5b4462ec9277c49f8b7530faa6ed7a5672880b18
Raphael Henrique Fernandes Rocha <rhfrocha@prodam.sp.gov.br>
2024-11-12 11:59:50 -0300

    Control buttons styling

--- src/components/Controle/index.js
+++ src/components/Controle/index.js
@@ -1,13 +1,29 @@
+/* eslint-disable */
 export default {
     name: 'controle',
     components: {},
-    data: {
-    
+    data() {
+        return {
+            start: false,
+            backgroundColor: '',
+            width: '',
+            color: ''
+        };
     },
     methods: {
+        botaoFuncional() {
+            this.start = !this.start
+            console.log(this.start)
+        }
     },
     computed: {
-
+        botaoInicial() {
+            return {
+                backgroundColor: 'rgba(47, 0, 255, 0.63)',
+                width: '220px',
+                color: 'white'
+            }
+        }
     },
     watch: {
 
--- src/components/Controle/index.vue
+++ src/components/Controle/index.vue
@@ -1,10 +1,11 @@
 <!-- eslint-disable -->
 <template>
   <div id="controle">
-    <button>Ataque</button>
-    <button>Ataque Especial</button>
-    <button>Curar</button>
-    <button>Desistir</button>
+      <button :style="botaoInicial" type="button" v-if="!start" @click="botaoFuncional">INICIAR NOVO JOGO</button>
+      <button type="button" v-if="start">ATAQUE</button>
+      <button type="button" v-if="start">ATAQUE ESPECIAL</button>
+      <button type="button" v-if="start">CURAR</button>
+      <button type="button" v-if="start">DESISTIR</button>
   </div>
 </template>
   
@@ -14,15 +15,17 @@
 #controle {
   display: flex;
   justify-content: space-around;
+  align-items: center;
   flex-wrap: wrap;
   width: 90%;
-  height: 100px;
+  height: 120px;
   box-shadow: 4px 4px 4px 4px #ccc;
 }
 button {
-  width: 90px;
+  width: 110px;
   height: 50px;
-  margin-top: 20px;
+  font-size: 20px;
+  box-shadow: 2px 2px 2px 2px #ccc;
   border: none;
   border-radius: 5px;
 }
@@ -33,10 +36,15 @@ button:nth-child(1) {
 button:nth-child(2) {
   background-color: rgba(197, 194, 1, 0.863);
   color: black;
-  width: 110px;
+  width: 200px;
 }
 button:nth-child(3) {
   background-color: rgb(0, 156, 0);
   color: white;
+}
+button:hover {
+  cursor: pointer;
+  box-shadow: none;
+  margin: 2px
 }
 </style>
